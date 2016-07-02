e5aad3b2d46c94916003811f22a22ba70046d723
aaa <kuangben2001@163.com>
2016-07-02 17:26:16 +0800

    修改框架

---
+++ Client/MyGame-Lua/frameworks/runtime-src/Classes/MyCC/Tools/UtilApi.cpp
@@ -0,0 +1,2 @@
+#include "UtilApi.h"
+
---
+++ Client/MyGame-Lua/frameworks/runtime-src/Classes/MyCC/Tools/UtilApi.h
@@ -0,0 +1,16 @@
+#ifndef __UtilApi_H__
+#define __UtilApi_H__
+
+#include "cocos2d.h"
+
+USING_NS_CC;
+
+class UtilApi
+{
+protected:
+
+public:
+
+};
+
+#endif
