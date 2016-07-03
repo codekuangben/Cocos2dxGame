fe12cf9458677cdf6cda6ac90b12db19324a01bb
codekuangben <kuangben2001@163.com>
2016-07-03 21:53:12 +0800

    提交代码

--- Client/MyGame-Lua/frameworks/runtime-src/Classes/MyCC/Tools/UtilApi.cpp
+++ Client/MyGame-Lua/frameworks/runtime-src/Classes/MyCC/Tools/UtilApi.cpp
@@ -2,10 +2,10 @@
 
 Size UtilApi::getStageVisibleSize()
 {
-
+	return Director::getInstance()->getVisibleSize();
 }
 
 Vec2 UtilApi::getStageVisibleOrigin()
 {
-
+	return Director::getInstance()->getVisibleOrigin();
 }
